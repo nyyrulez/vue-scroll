f649023d36c230aa9be77149013a3478d68df928
RJ <120344418+RandyZJin@users.noreply.github.com>
2025-11-20 18:51:48 +0000

    copy from demo of scrollyvideo

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.vue';
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+<div class="scrolly-video-container">
+  <ScrollyVideo src="https://scrollyvideo.js.org/goldengate.mp4" />
+</div>
+</template>
+
+<style scoped>
+.scrolly-video-container {
+  height: 300vh;
 }
 </style>
